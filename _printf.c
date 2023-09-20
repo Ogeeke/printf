39ee4c29e1849ff20d80124babe3d7dc5e98a6ae
chuksebu <chukwuike.ebuzome@gmail.com>
2023-09-19 17:37:56 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,67 +1,67 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Custom printf function
+ * @format: The format string
+ * @...: Variable number of arguments
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    va_list args;
+    int count = 0;
 
-	if (format == NULL)
-		return (-1);
+    va_start(args, format);
 
-	va_start(list, format);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1); // Print non-% characters
+            count++;
+        }
+        else
+        {
+            format++; // Move to the character after '%'
+            switch (*format)
+            {
+                case 'c':
+                {
+                    char c = va_arg(args, int); // Get the character argument
+                    write(1, &c, 1); // Print the character
+                    count++;
+                    break;
+                }
+                case 's':
+                {
+                    char *str = va_arg(args, char *); // Get the string argument
+                    while (*str)
+                    {
+                        write(1, str, 1); // Print each character of the string
+                        str++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%':
+                {
+                    write(1, "%", 1); // Print a literal %
+                    count++;
+                    break;
+                }
+                default:
+                    write(1, "%", 1); // Print the original % and the unknown character
+                    write(1, format, 1);
+                    count += 2;
+                    break;
+            }
+        }
+        format++; // Move to the next character in the format string
+    }
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+    va_end(args);
+    return count;
 }
 
